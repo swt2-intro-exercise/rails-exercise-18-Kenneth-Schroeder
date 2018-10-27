7839c60c01c934b9714a6389af136f4063e29cc1
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2018-10-27 16:09:05 +0200

    new_author page gets directed to root for now and thus renders correctly

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get '/new_author' => 'home#index'
+
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -1,7 +1,8 @@
 require 'rails_helper'
 
 describe "New author page", type: :feature do
-  
+  new_author_path = "new_author"
+
   it "should render withour error" do
     visit new_author_path
   end
