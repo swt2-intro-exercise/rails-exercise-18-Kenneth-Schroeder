15aecd5cc1208885fd46fa7b8fe6d8591307286e
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2018-10-29 23:44:45 +0100

    issue 9

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -1,10 +1,15 @@
 require 'rails_helper'
 
 describe "Authors Model Tests", type: :model do #or :type => :model
-  it "should be valid" do
-  	author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'aHomepage')
-  	expect(author.first_name).to eq('Alan')
-  	expect(author.last_name).to eq('Turing')
-  	expect(author.homepage).to eq('aHomepage')
+  it "should have a first name, last name and homepage" do
+  	@author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'aHomepage')
+  	expect(@author.first_name).to eq('Alan')
+  	expect(@author.last_name).to eq('Turing')
+  	expect(@author.homepage).to eq('aHomepage')
+  end
+
+  it "should only be valid with a last name" do
+  	@author = Author.new(first_name: 'Alan', last_name: '', homepage: 'aHomepage')
+  	expect(@author).to_not be_valid
   end
 end
