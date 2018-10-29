4721cba080055aabeafdac7a0b360a382958af08
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2018-10-29 23:36:03 +0100

    issue 8

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -2,9 +2,20 @@ require 'rails_helper'
 
 describe "Authors index (overview) page", type: :feature do
 
-  it "should display all authors from the database" do
+  it "should have a link to create a new author" do
   	visit authors_path
-    expect(page).to have_link 'New', href: new_author_path
+    expect(page).to have_link 'New Author', href: new_author_path
+  end
+
+  it "should have links to all authors pages" do
+  	@alan = FactoryBot.create :author
+  	visit authors_path
+  	expect(page).to have_link 'Show', href: author_path(@alan)
+  end
+
+  it "should have a table" do
+  	visit authors_path
+  	expect(page).to have_table
   end
 
 end
