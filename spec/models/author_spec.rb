e5f2b18a3a1e879b8f0d97553bdf78ba47b6bc76
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2018-10-29 21:45:25 +0100

    issue 4

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 describe "Authors Model Tests", :type => :model do
   it "should be valid" do
-  	author = Author.new(first_name: 'aFirstName', last_name: 'aLastName', homepage: 'aHomepage')
-  	expect(author.first_name).to eq('aFirstName')
-  	expect(author.last_name).to eq('aLastName')
+  	author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'aHomepage')
+  	expect(author.first_name).to eq('Alan')
+  	expect(author.last_name).to eq('Turing')
   	expect(author.homepage).to eq('aHomepage')
   end
 end
