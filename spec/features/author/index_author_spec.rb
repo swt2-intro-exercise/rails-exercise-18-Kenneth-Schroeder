2f8e49e655e8e7db20aba352a5567f88b891aad4
Kenneth-Schroeder <kenneth.schroeder@student.hpi.de>
2018-10-30 12:08:42 +0100

    issue 12

--- spec/features/author/index_author_spec.rb
+++ spec/features/author/index_author_spec.rb
@@ -7,10 +7,16 @@ describe "Authors index (overview) page", type: :feature do
     expect(page).to have_link 'New Author', href: new_author_path
   end
 
-  it "should have links to all authors pages" do
+  it "should have links to all authors home pages" do
   	@alan = FactoryBot.create :author
   	visit authors_path
   	expect(page).to have_link 'Show', href: author_path(@alan)
+  end
+
+  it "should have links to all authors editing pages" do
+    @alan = FactoryBot.create :author
+    visit authors_path
+    expect(page).to have_link 'Edit', href: edit_author_path(@alan)
   end
 
   it "should have a table" do
